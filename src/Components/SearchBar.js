fff41aefefc4f040c6e5f6d9455c4c199898b3a9
Knight <mayanks365@gmail.com>
2021-01-29 22:51:29 +0530

    controlled element

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+class SearchBar extends Component {
+
+    render (  ){
+        return(
+            <div className="ui segment">
+                <form className="ui form" >
+                    <div className="field">
+                        <label >Image Search</label>
+                        <input type="text" 
+                        onChange={
+                            (e) => console.log(e.target.value) 
+                        }/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
+
