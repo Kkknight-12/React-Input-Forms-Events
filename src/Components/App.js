4125328acd365b37c4769e06e59f9f44a78e5432
Knight <mayanks365@gmail.com>
2021-01-30 02:09:58 +0530

    updating axios codes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,27 @@
 import { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends Component {
 
   onSearchSubmit(term){
-    console.log(term);
-
+    
+    // take two argument
+    // first is path of request
+    // second is object that customise the request
+    axios.get( 'https://api.unsplash.com/search/photos', {
+      // params defines different query parameter we want to add into the request
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID _VtG5n6vKkM5SjzwCyxZler0e-VsjL3QDeo_p-w0av0'
+      }
+    })
   }
 
   render() {
 
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
-        {/* passing function onSearchSubmit as prop */}
-        {/* prop name onSubmit is user defined name, so can be anything */}
       <SearchBar onSubmit={ this.onSearchSubmit }/>
       </div>
     )
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -7,8 +7,6 @@ class SearchBar extends Component {
     onFormSubmit = (e) => {
         e.preventDefault();
 
-        // to reference the function onSearchSubmit inside prop from App.js  
-        // we need to mention the name onSubmit by which it was saved
         this.props.onSubmit( this.state.term )
     }
 
@@ -19,8 +17,6 @@ class SearchBar extends Component {
                     <div className="field">
                         <label >Image Search</label>
                         <input type="text" 
-                        // example of controlled element
-                        // now React control the storage
                         value={ this.state.term }
                         onChange={
                             (e) =>  this.setState({ term: e.target.value })
@@ -31,10 +27,5 @@ class SearchBar extends Component {
         )
     }
 }
-// now whenever use submit form we are going to run onFormSubmit() from line 18
-// which will take the event and prevent default that would have caused 
-// the page to refesh itself
-// then we look at props object line 12 and call the function which was passed into 
-// onSubmit prop in App.js and invoke that function onSearchSubmit()
-//  with this.state.term
+
 export default SearchBar;
