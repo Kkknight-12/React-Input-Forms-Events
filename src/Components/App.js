d4e9ced51a99b4beb1ec6a4e0a49ab8c3981fc85
Knight <mayanks365@gmail.com>
2021-01-30 02:23:34 +0530

    changing from promise to async await

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,19 +4,23 @@ import SearchBar from './SearchBar'
 
 class App extends Component {
 
-  onSearchSubmit(term){
+// async key is written in front of method name
+  async onSearchSubmit(term){
     
-    // take two argument
-    // first is path of request
-    // second is object that customise the request
-    axios.get( 'https://api.unsplash.com/search/photos', {
+    // we write await keyword in front of thing that we want to return
+    // or thing that take sometime here network request
+    // whatever it return is stored in some variable
+    const response = await axios.get( 'https://api.unsplash.com/search/photos', {
       // params defines different query parameter we want to add into the request
       params: { query: term },
       headers: {
         Authorization: 'Client-ID _VtG5n6vKkM5SjzwCyxZler0e-VsjL3QDeo_p-w0av0'
       }
-    }) // request made by axios returns a promise so we can chain .then()
-    .then( (response) => { console.log( response.data.results ) } )
+    }) 
+
+    // than we can work with the variable later on
+    // here showing output
+    console.log( response.data.results )
 
   }
 
