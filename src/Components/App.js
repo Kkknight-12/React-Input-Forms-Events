db03b8cabe45f36ebf7be80bca75bbb2bfffdedc
Knight <mayanks365@gmail.com>
2021-01-30 11:39:34 +0530

    adding state and creating async arrow function

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,23 +4,34 @@ import SearchBar from './SearchBar'
 
 class App extends Component {
 
-// async key is written in front of method name
-  async onSearchSubmit(term){
+  // Initializing state
+  // whenever we expect state property to  contain object/array
+  // we usually intializse property to be an empty object/array
+  // that allow us to call  certain functions on this state property
+  // without getting error like funciton not found etc..
+  state =  { images: [ ] };
+
+  // making the function arrow to use this.setState
+  onSearchSubmit = async (term) => {
     
-    // we write await keyword in front of thing that we want to return
-    // or thing that take sometime here network request
-    // whatever it return is stored in some variable
     const response = await axios.get( 'https://api.unsplash.com/search/photos', {
-      // params defines different query parameter we want to add into the request
+
       params: { query: term },
       headers: {
         Authorization: 'Client-ID _VtG5n6vKkM5SjzwCyxZler0e-VsjL3QDeo_p-w0av0'
       }
     }) 
 
-    // than we can work with the variable later on
-    // here showing output
-    console.log( response.data.results )
+    // map function is built into array object in Js
+    // if you call map on value of null you will end up with error message
+    // but if you can call map function on empty array it will run
+    // this.state.images.map
+    // commenting it out as it is not need here
+
+    this.setState( { images: response.data.results });
+    // after making request over to splash we get repsone we pull results
+    // we set it on our state object and that will cause our component to
+    // re-render
 
   }
 
@@ -29,6 +40,11 @@ class App extends Component {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
       <SearchBar onSubmit={ this.onSearchSubmit }/>
+
+      {/* settting the images to empty array helps here too 
+      as if we have set it to null, length of images would give 
+      error message said cannot access property of length null */}
+      Found : { this.state.images.length } images
       </div>
     )
   }
